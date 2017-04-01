b9cbb292247dbde5c7ecfdf2ddaa03fbd8d1d308
Amanda Beiner <aebeiner@gmail.com>
2017-03-31 21:30:14 -0400

    navbar styling

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router';
 const Navbar = props => {
   return(
-    <div>
-      <ul>
+    <div className="navbar">
+      <ul className="links">
         <li><Link to='/'>Home</Link></li>
         <li><a href='https://github.com/amandabeiner' target="_blank">GitHub</a></li>
         <li><Link to='/contact'>Contact</Link></li>
