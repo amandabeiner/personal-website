28ad48db36f4b14e65a83200f8d5867f01bc1400
Amanda Beiner <aebeiner@gmail.com>
2017-07-09 21:16:14 -0400

    test PugContainer

--- app/test/containers/PugsContainerSpec.js
+++ app/test/containers/PugsContainerSpec.js
@@ -1,5 +1,5 @@
 import PugsContainer from 'containers/PugsContainer';
-
+import Pug from 'components/Pug'
 describe('PugsContainer', () => {
   let wrapper;
 
@@ -8,12 +8,45 @@ describe('PugsContainer', () => {
       return(createResponseFromFixture('pugs'));
     })
 
+    spyOn(PugsContainer.prototype, 'getNewPugGif').and.callThrough();
+
     wrapper = mount(
       <PugsContainer/>
     )
   });
 
   it('should render a Pug component', () => {
-    expect(true).toEqual(true)
+    expect(wrapper.find(Pug)).toBePresent();
   });
+
+  it('should initialize with the specified state', () => {
+    expect(wrapper.state()).toEqual({
+      gifUrl: {}
+    })
+  })
+
+  it('should pass down different props when the gif stored in state changes', () => {
+    wrapper.setState({ gifUrl: 'new url' })
+
+    expect(wrapper.find(Pug)).toHaveProp('gif', 'new url');
+  })
+
+  describe('getNewPugGif', () => {
+    it('should be invoked when the button on the Pug component is clicked', () => {
+      let nextPugButton = wrapper.find(Pug).find('button')
+      nextPugButton.simulate('click')
+      expect(PugsContainer.prototype.getNewPugGif).toHaveBeenCalled()
+    })
+
+    it('should set the state of the PugContainer to the new gif', done => {
+      wrapper.find(Pug).props().onClick();
+
+      setTimeout(() => {
+        expect(wrapper.state()).toEqual({
+          gifUrl: 'http://media3.giphy.com/media/11kgieHVYW53lC/giphy.gif'
+        })
+        done();
+      }, 0);
+    })
+  })
 })
