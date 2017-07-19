2d10fb5688cb122dbb7d4f5acb175b020bfc17c2
Amanda Beiner <aebeiner@gmail.com>
2017-07-19 19:03:18 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Project.create(name: "Pourmans Coffee", description: "Custom web app for local coffee business", image: "http://res.cloudinary.com/dpuzgzqir/image/upload/v1500503808/Screen_Shot_2017-07-19_at_6.35.12_PM_wgp6cu.png", web_link: "http://pourmans-coffee.herokuapp.com", github_link: "http://github.com/michaelgt04/pourmans-coffee")
+Project.create(name: "Pourmans Coffee", description: "Custom web app for local coffee business", image: "http://res.cloudinary.com/dpuzgzqir/image/upload/v1500505355/Screen_Shot_2017-07-19_at_7.01.56_PM_iuxszt.png", web_link: "http://pourmans-coffee.herokuapp.com", github_link: "http://github.com/michaelgt04/pourmans-coffee")
 
 Project.create(name: "Hack Back", description: "Connecting non-profits with socially-minded technologists", image: "http://res.cloudinary.com/dpuzgzqir/image/upload/v1499083881/Screen_Shot_2017-07-03_at_8.04.58_AM_xyklue.png", github_link: "http://github.com/amandabeiner/hack-back")
 
