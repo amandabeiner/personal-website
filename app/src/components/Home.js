fabeb4e1e803f932e87b9d0296c4322ab2732b0c
Amanda Beiner <aebeiner@gmail.com>
2018-09-11 13:58:10 -0400

    Finish home section

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import ScrollableAnchor from 'react-scrollable-anchor';
 
-const Home = props => {
-  return(
-      <div className="home">
-        <ScrollableAnchor id={'home'}>
+/* <ScrollableAnchor id={'home'}>
           <div className="home-anchor">
           </div>
-        </ScrollableAnchor>
-        <div className="home-content">
-          <div className="home-text">
-            <h1 className="name">Hi, {"I'm"} Amanda</h1>
-            <hr></hr>
-            <p className="bio">
-              Software developer, JavaScript enthusiast, full-time admirer of squishy-faced dogs
-            </p>
-          </div>
+        </ScrollableAnchor> */
+const Home = props => {
+  return (
+    <div className="home">
+      <div className="home-content">
+        <div className="home-text">
+          <h1 className="name">Hi, {"I'm"} Amanda</h1>
+          <hr></hr>
+          <p className="bio">
+            Software developer, JavaScript enthusiast, full-time admirer of squishy-faced dogs
+          </p>
+        </div>
+        <div className="arrow-down">
+          <a href="#portfolio">
+            <i className="fa fa-chevron-down bounce"></i>
+          </a>
         </div>
       </div>
+    </div>
   )
 }
 
