df8fce834f8ba69f4145ae3cb2fa9ae2e7693b57
Amanda Beiner <aebeiner@gmail.com>
2018-09-10 23:41:19 -0400

    Redo home section base

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -8,13 +8,14 @@ const Home = props => {
           <div className="home-anchor">
           </div>
         </ScrollableAnchor>
-          <div className="image-wrapper">
-            <img className="profile-photo" src="https://avatars1.githubusercontent.com/u/21161427?v=3&s=460" />
+        <div className="home-content">
+          <div className="home-text">
+            <h1 className="name">Hi, {"I'm"} Amanda</h1>
+            <hr></hr>
+            <p className="bio">
+              Software developer, JavaScript enthusiast, full-time admirer of squishy-faced dogs
+            </p>
           </div>
-        <div className="home-text">
-          <h1 className="name">Amanda Beiner</h1>
-          <hr></hr>
-          <p className="bio">A Boston-based software developer, amateur yogi, and full-time admirer of squishy-faced dogs</p>
         </div>
       </div>
   )
